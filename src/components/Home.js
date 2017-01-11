7808a8d29b64e6d018efc168e8d8b80c54933c5b
Jacob Childress <jacobc@gmail.com>
2017-01-10 20:12:12 -0600

    Make Home a functional stateless component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
-import objectPath from 'object-path';
+import React from 'react';
 import Loading from './Loading';
 import UserSummary from './UserSummary';
 import UserDetails from './UserDetails';
 import Logout from './Logout';
+import Scim from '../util/Scim';
 import { SCHEMA } from '../Config';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.dump = this.dump.bind(this);
-  }
-
-  dump(obj) {
-    return JSON.stringify(obj);
-  }
-
-  render() {
-    return this.props.isLoading === true
-        ? (
-            <Loading/>
-        )
-        : (
-            <div>
-              <UserSummary
-                  username={objectPath.get(this.props.data, SCHEMA.username)}
-                  name={objectPath.get(this.props.data, SCHEMA.fullName)}
-              />
-              <UserDetails data={this.props.data}/>
-              <Logout/>
-            </div>
-        );
-  }
-}
+const Home = props => {
+  const scimUser = new Scim(props.data);
+  return props.isLoading === true
+      ? (
+          <Loading/>
+      )
+      : (
+          <div>
+            <UserSummary
+                username={scimUser.getValue(SCHEMA.username)}
+                name={scimUser.getValue(SCHEMA.fullName)}
+            />
+            <UserDetails data={props.data}/>
+            <Logout/>
+          </div>
+      );
+};
 
 Home.propTypes = {
   isLoading: React.PropTypes.bool.isRequired,
